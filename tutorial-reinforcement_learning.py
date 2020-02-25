a5c6b46fbdd570c46a10a3b59710e6d328e934df
Harold <harolds98@live.be>
2020-02-26 00:18:08 +0100

    adding first sample of reinforcement learning

--- tutorial-reinforcement_learning.py
+++ tutorial-reinforcement_learning.py
@@ -11,6 +11,7 @@ def step(self, action):
 '''
 
 import glob
+import math
 import os
 import sys
 
@@ -38,6 +39,8 @@ SHOW_PREVIEW = False
 IMG_WIDTH = 640
 IMG_HEIGHT = 480
 
+SECONDS_PER_EPISODE = 10
+
 class CarEnv:
     SHOW_CAM = SHOW_PREVIEW
     # full turns
@@ -61,47 +64,54 @@ class CarEnv:
 
         # The world contains the list blueprints that we can use for adding new
         # actors into the simulation.
-        blueprint_library = self.world.get_blueprint_library()
+        self.blueprint_library = self.world.get_blueprint_library()
 
         # Now let's filter all the blueprints of type 'vehicle' and choose one
         # at random.
         #print(blueprint_library.filter('vehicle'))
-        self.model_3 = blueprint_library.filter('model3')[0]
+        self.model_3 = self.blueprint_library.filter('model3')[0]
 
     def reset(self):
         self.collision_hist = []
         self.actor_list = []
 
+        # spawn point
         self.transform = random.choice(self.world.get_map().get_spawn_points())
         self.vehicle = self.world.spawn_actor(self.model_3, self.transform)
         self.actor_list.append(self.vehicle)
 
-        self.rgb_cam = self.world.get_blueprint_library().find('sensor.camera.rgb')
+        self.rgb_cam = self.blueprint_library().find('sensor.camera.rgb')
 
         self.rgb_cam.set_attribute('image_size_x', f'{self.im_width}')
         self.rgb_cam.set_attribute('image_size_y', f'{self.im_height}')
         self.rgb_cam.set_attribute('fov', '110')
 
-
+        # specify spawn point of camera
         transform = carla.Transform(carla.Location(x=2.5, z=0.7))
 
         self.sensor = self.world.spawn_actor(self.rgb_cam, transform, attach_to=self.vehicle)
 
         self.actor_list.append(self.sensor)
+        # do something with this sensor, in this case listen for its imagery
         self.sensor.listen(lambda data: self.process_img(data))
 
+        # this 2 lines need improvement as it takes a lot of time
         self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=0.0))
 
-        time.sleep(4) # sleep to get things started and to not detect a collision when the car spawns/falls from sky.
+        # sleep to get things started (sensors can take time to initialize and return values) and
+        # to not detect a collision when the car spawns/falls from sky. which happens quite often
+        time.sleep(4)
 
         colsensor = self.world.get_blueprint_library().find('sensor.other.collision')
         self.colsensor = self.world.spawn_actor(colsensor, transform, attach_to=self.vehicle)
         self.actor_list.append(self.colsensor)
         self.colsensor.listen(lambda event: self.collision_data(event))
 
+        # make sure to wait until all is setup
         while self.front_camera is None:
             time.sleep(0.01)
 
+        # episode length
         self.episode_start = time.time()
 
         self.vehicle.apply_control(carla.VehicleControl(brake=0.0, throttle=0.0))
@@ -115,27 +125,30 @@ class CarEnv:
         i = np.array(image.raw_data)  # convert to an array
         #np.save("iout.npy", i)
         i2 = i.reshape((self.im_height, self.im_width, 4)) # was flattened, so we're going to shape it.
-        i3 = i2[:, :, :3] # remove the alpha (basically, remove the 4th index  of every pixel. Converting RGBA to RGB)
+        i3 = i2[:, :, :3]  # remove the alpha (basically, remove the 4th index  of every pixel. Converting RGBA to RGB)
         if self.SHOW_CAM:
-            cv2.imshow("",i3) # show it.
+            cv2.imshow("", i3)  # show it.
             cv2.waitKey(1)
-        self.front_camera = i3 # normalize
+        self.front_camera = i3  # normalize
 
+    # methods takes an action and then returns an observation, reward, done, any_extra_info as for the usual
+    # learning paradigm
     def step(self, action):
         '''
         For now let's just pass steer left, center, right?
         0, 1, 2
         '''
         if action == 0:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0))
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=-1 * self.STEER_AMT))
         if action == 1:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=-1*self.STEER_AMT))
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0))
         if action == 2:
             self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=1*self.STEER_AMT))
 
         v = self.vehicle.get_velocity()
         kmh = int(3.6 * math.sqrt(v.x**2 + v.y**2 + v.z**2))
 
+        # for now, if any collision is detected, we stop
         if len(self.collision_hist) != 0:
             done = True
             reward = -200
