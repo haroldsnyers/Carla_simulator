e7f6319f5cc1f610e507b24b83c0c549a34c0f0a
Harold <harolds98@live.be>
2020-03-09 01:42:38 +0100

    working on carla tutorial

--- tutorial-reinforcement_learning.py
+++ tutorial-reinforcement_learning.py
@@ -33,6 +33,11 @@ import random
 import time
 import numpy as np
 import cv2
+import keras.backend.tensorflow_backend as backend
+import tensorflow as tf
+from threading import Thread
+
+from tutorial_reninforcement_learning_1 import DQNAgent
 
 # whether or not we want to display the camera as it will take resources
 SHOW_PREVIEW = False
@@ -40,6 +45,19 @@ IMG_WIDTH = 640
 IMG_HEIGHT = 480
 
 SECONDS_PER_EPISODE = 10
+
+MEMORY_FRACTION = 0.8
+EPISODES = 100
+epsilon = 1
+EPSILON_DECAY = 0.95 ## 0.9975 99975
+MIN_EPSILON = 0.001
+
+MODEL_NAME = "Xception"
+
+MIN_REWARD = -200
+
+AGGREGATE_STATS_EVERY = 10
+
 
 class CarEnv:
     SHOW_CAM = SHOW_PREVIEW
@@ -163,3 +181,115 @@ class CarEnv:
             done = True
 
         return self.front_camera, reward, done, None
+
+
+if __name__ == '__main__':
+    FPS = 60
+    # For stats
+    # starting with high epsilon -> high probability that we will be choosing an action randomly,
+    # rather than predicting it with our neural network. A random choice is going to be much faster than a predict
+    # operation, so we can arbitrarily delay this by setting some sort of general FPS
+    ep_rewards = [-200]
+
+    # For more repetitive results
+    random.seed(1)
+    np.random.seed(1)
+    tf.set_random_seed(1)
+
+    # Memory fraction, used mostly when training multiple agents
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=MEMORY_FRACTION)
+    backend.set_session(tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)))
+
+    # Create models folder
+    if not os.path.isdir('models'):
+        os.makedirs('models')
+
+    # Create agent and environment
+    agent = DQNAgent()
+    env = CarEnv()
+
+    # Start training thread and wait for training to be initialized
+    trainer_thread = Thread(target=agent.train_in_loop, daemon=True)
+    trainer_thread.start()
+    while not agent.training_initialized:
+        time.sleep(0.01)
+
+    # Initialize predictions - first prediction takes longer as of initialization that has to be done
+    # It's better to do a first prediction then before we start iterating over episode steps
+    agent.get_qs(np.ones((env.im_height, env.im_width, 3)))
+
+    # Iterate over episodes
+    # Iterate over episodes
+    for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
+        # try:
+
+        env.collision_hist = []
+
+        # Update tensorboard step every episode
+        agent.tensorboard.step = episode
+
+        # Restarting episode - reset episode reward and step number
+        episode_reward = 0
+        step = 1
+
+        # Reset environment and get initial state
+        current_state = env.reset()
+
+        # Reset flag and start iterating until episode ends
+        done = False
+        episode_start = time.time()
+
+        # Play for given number of seconds only
+        while True:
+
+            # This part stays mostly the same, the change is to query a model for Q values
+            if np.random.random() > epsilon:
+                # Get action from Q table
+                action = np.argmax(agent.get_qs(current_state))
+            else:
+                # Get random action
+                action = np.random.randint(0, 3)
+                # This takes no time, so we add a delay matching 60 FPS (prediction above takes longer)
+                time.sleep(1 / FPS)
+
+            new_state, reward, done, _ = env.step(action)
+
+            # Transform new continous state to new discrete state and count reward
+            episode_reward += reward
+
+            # Every step we update replay memory
+            agent.update_replay_memory((current_state, action, reward, new_state, done))
+
+            current_state = new_state
+            step += 1
+
+            if done:
+                break
+
+        # End of episode - destroy agents
+        for actor in env.actor_list:
+            actor.destroy()
+
+        # Append episode reward to a list and log stats (every given number of episodes)
+        ep_rewards.append(episode_reward)
+        if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+            average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:]) / len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward,
+                                           epsilon=epsilon)
+
+            # Save model, but only when min reward is greater or equal a set value
+            if min_reward >= MIN_REWARD:
+                agent.model.save(
+                    f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+
+        # Decay epsilon
+        if epsilon > MIN_EPSILON:
+            epsilon *= EPSILON_DECAY
+            epsilon = max(MIN_EPSILON, epsilon)
+
+    # Set termination flag for training thread and wait for it to finish
+    agent.terminate = True
+    trainer_thread.join()
+    agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
---
+++ tutorial_reninforcement_learning_1.py
@@ -0,0 +1,134 @@
+import numpy as np
+import time
+import random
+
+from collections import deque
+from keras.applications.xception import Xception
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.optimizers import Adam
+from keras.models import Model
+from TensorBoardClass import ModifiedTensorBoard
+import tensorflow as tf
+
+IM_WIDTH = 640
+IM_HEIGHT = 480
+
+REPLAY_MEMORY_SIZE = 5_000
+MIN_REPLAY_MEMORY_SIZE = 1_000
+MINIBATCH_SIZE = 16
+PREDICTION_BATCH_SIZE = 1
+TRAINING_BATCH_SIZE = MINIBATCH_SIZE // 4
+UPDATE_TARGET_EVERY = 5
+MODEL_NAME = "Xception"
+
+MIN_REWARD = -200
+
+DISCOUNT = 0.99
+
+
+class DQNAgent:
+    def __init__(self):
+        self.model = self.create_model()
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+
+        # train from randomly selected data from replay memory
+        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+
+        self.tensorboard = ModifiedTensorBoard(log_dir=f"logs/{MODEL_NAME}-{int(time.time())}")
+        self.target_update_counter = 0  # will track when it's time to update the target model
+        self.graph = tf.get_default_graph()
+
+        self.terminate = False  # Should we quit?
+        self.last_logged_episode = 0
+        self.training_initialized = False  # waiting for TF to get rolling
+
+    def create_model(self):
+        # could use another one
+        base_model = Xception(weights=None, include_top=False, input_shape=(IM_HEIGHT, IM_WIDTH, 3))
+
+        x = base_model.output
+        # adding to out layer
+        x = GlobalAveragePooling2D()(x)
+
+        # adding the 3 neuron output (each possible action for the agent to take)
+        predictions = Dense(3, activation="linear")(x)
+        model = Model(inputs=base_model.input, outputs=predictions)
+        model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=["accuracy"])
+        return model
+
+    def update_replay_memory(self, transition):
+        # transition = (current_state, action, reward, new_state, done)
+        self.replay_memory.append(transition)
+
+    # we only want to train if we have a bare minimum of sample in replay memory
+    def train(self):
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            return
+
+        # first grab a random minibatch
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+
+        # grab our current and future q values
+        # Recall a transition is: transition = (current_state, action, reward, new_state, done)
+        current_states = np.array([transition[0] for transition in minibatch])/255
+        with self.graph.as_default():
+            current_qs_list = self.model.predict(current_states, PREDICTION_BATCH_SIZE)
+
+        new_current_states = np.array([transition[3] for transition in minibatch])/255
+        with self.graph.as_default():
+            future_qs_list = self.target_model.predict(new_current_states, PREDICTION_BATCH_SIZE)
+
+        # create our input X and output Y
+        X = []
+        y = []
+
+        for index, (current_state, action, reward, new_state, done) in enumerate(minibatch):
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + DISCOUNT * max_future_q
+            else:
+                new_q = reward
+
+            current_qs = current_qs_list[index]
+            current_qs[action] = new_q
+
+            X.append(current_state)
+            y.append(current_qs)
+
+        log_this_step = False
+        if self.tensorboard.step > self.last_logged_episode:
+            log_this_step = True
+            self.last_log_episode = self.tensorboard.step
+
+        # fitting
+        with self.graph.as_default():
+            # we're setting the tensorboard callback, only if log_this_step is true
+            self.model.fit(np.array(X)/255, np.array(y), batch_size=TRAINING_BATCH_SIZE, verbose=0, shuffle=False, callbacks=[self.tensorboard] if log_this_step else None)
+
+        if log_this_step:
+            self.target_update_counter += 1
+
+        # look if it's time to update model
+        if self.target_update_counter > UPDATE_TARGET_EVERY:
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_update_counter = 0
+
+    # method to get q values, thus make a prediction
+    def get_qs(self, state):
+        return self.model.predict(np.array(state).reshape(-1, *state.shape)/255)[0]
+
+    def train_in_loop(self):
+        X = np.random.uniform(size=(1, IM_HEIGHT, IM_WIDTH, 3)).astype(np.float32)
+        y = np.random.uniform(size=(1, 3)).astype(np.float32)
+        with self.graph.as_default():
+            self.model.fit(X,y, verbose=False, batch_size=1)
+
+        self.training_initialized = True
+
+        while True:
+            if self.terminate:
+                return
+            self.train()
+            time.sleep(0.01)
+
